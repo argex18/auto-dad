ceda57eef2aa417e3b1e2f36b78650aca4d9dd2f
argex18 <alessandro.argentini@iticopernico.it>
2021-02-10 14:15:06 +0100

    Project started at 10/02/2021:14.12

---
+++ README.md
@@ -0,0 +1,7 @@
+# Script to download the dad lessons loaded to google classroom automatically
+
+Thought to work indipendently or be executed from another script in a more complex scenario.
+
+# Development in progress...
+
+Future updates will be uploaded here.
---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+
+In this module will be executed the main script.
+
+"""
+
+try:
+    import sys
+except ImportError:
+    print('error: import error for sys module...killing script execution')
+    __name__ = None
+
+try:
+    from traceback import print_exc
+except ImportError:
+    sys.exit('error: import failed for one or more modules')
+
+def main():
+    #
+    # script execution
+    #
+
+try:
+    if __name__ == '__main__':
+        main()
+    else:
+        raise RuntimeError('error: this module cannot be loaded from another one')
+except RuntimeError:
+    print_exc()
